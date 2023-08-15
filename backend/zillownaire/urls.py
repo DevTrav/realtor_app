7db2504573fc421cb4a91384ad55f39c6e530f5e
Travers La Ville <traverslaville@traverss-mbp.lan>
2023-08-14 23:47:30 -0500

    Adding 404 view and regex for react routing

--- backend/zillownaire/urls.py
+++ backend/zillownaire/urls.py
@@ -23,9 +23,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, ToeknRefreshView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshViews.as_view(), name='token_refresh'),
-    path('api/accounts', include(accounts.urls))
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/accounts', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [ re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
