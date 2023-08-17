621de53d2e4e81d84e39bc62a6bd0cb25a133f7f
Travers La Ville <traverslaville@traverss-mbp.lan>
2023-08-17 00:55:35 -0500

    Corrected syntax error

--- backend/zillownaire/settings.py
+++ backend/zillownaire/settings.py
@@ -122,13 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,   
+    'PAGE_SIZE': 3  
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
